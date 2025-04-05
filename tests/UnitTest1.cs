4369d38988512d2dae0b9038f66a84ce48fc43d0
Natalia <natalia@Natalias-MacBook-Air.local>
2025-04-05 14:15:11 +0200

    Add tests

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
+
+
+public class UnitTest2
+{
+    [Fact]
+    public void Test2_HelloWorld()
+    {
+        Assert.Equal("Hello world 1", "Hello world 1");
+    }
+}
